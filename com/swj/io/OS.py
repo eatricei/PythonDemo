3090560912c01ae442a43e881715e3492cb7c8d4
swj <1132331056@qq.com>
2017-12-23 16:36:55 +0800

    add missing modules

--- com/swj/io/OS.py
+++ com/swj/io/OS.py
@@ -7,12 +7,12 @@ print(os.environ.get('PATH'))
 print(os.environ.get('x', 'default'))
 
 print(os.path.abspath('.'))  # 查看当前目录的绝对路径
-print(os.path.join('test'))  # 合成路径
+print(os.path.join('com'))  # 合成路径
 os.path.split('')  # 拆分路径
 os.path.splitext('')  # 可以获得文件的扩展名
 
-# print(os.mkdir('test'))
-print(os.rmdir('test'))
+# print(os.mkdir('com'))
+print(os.rmdir('com'))
 
 os.rename('原名', '新名')
 os.remove('')
